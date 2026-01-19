ea767cc76740eadba5e806f6a16fd103a39a1c4a
Anatole HAGBE <ahagbes89@gmail.com>
2026-01-19 04:28:44 +0100

    Api (#1)
    * feat: Add new endpoint
    
    * feat: Add new endpoint

--- core/api.py
+++ core/api.py
@@ -5,3 +5,8 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def health(request):
     return Response({"message": "The TODO API is healthy!"}, status=200)
+
+
+@api_view(['GET'])
+def all_todos(request):
+    return Response({"message": "All TODOs"}, status=200)
--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from core import api
 
 urlpatterns = [
-    path("health", api.health, name="health")
+    path("health", api.health, name="health"),
+    path("all_todos", api.all_todos, name="all_todos"),
 ]
