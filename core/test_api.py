93ef9ad69d582890b0d6100bb39378ebe1655d62
Anatole HAGBE <ahagbes89@gmail.com>
2026-01-19 04:24:07 +0100

    feat: Add new endpoint

--- core/test_api.py
+++ core/test_api.py
@@ -8,3 +8,8 @@ class TestApi(APITestCase):
         response = self.client.get(reverse("health"))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()["message"], "The TODO API is healthy!")
+
+    def test_should_return_all_todos(self):
+        response = self.client.get(reverse("all_todos"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["message"], "All TODOs")
