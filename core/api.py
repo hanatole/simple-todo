8826d724542d65f60a151957b928359e514aedf5
Anatole HAGBE <ahagbes89@gmail.com>
2026-01-19 03:55:04 +0100

    cicd: fix typo

--- core/api.py
+++ core/api.py
@@ -4,4 +4,4 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def health(request):
-    return Response({"message": "The API is healthy!"}, status=200)
+    return Response({"message": "The TODO API is healthy!"}, status=200)
