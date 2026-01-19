e3cfc6988026488cb8d9d29b33a903ec2be00284
Anatole HAGBE <ahagbes89@gmail.com>
2026-01-19 16:14:57 +0100

    feat: Add api

--- core/api.py
+++ core/api.py
@@ -1,5 +1,9 @@
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from core.models import Todo
+from core.serializers import TodoSerializer
 
 
 @api_view(["GET"])
@@ -7,6 +11,8 @@ def health(request):
     return Response({"message": "The TODO API is healthy!"}, status=200)
 
 
-@api_view(["GET"])
-def all_todos(request):
-    return Response({"message": "All TODOs"}, status=200)
+class TodoViewSet(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        return Todo.objects.all()
